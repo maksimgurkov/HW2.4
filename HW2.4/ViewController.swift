0a67e192a8f15892c9dc0d653f3d6b9f3d54caee
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-02-06 10:24:46 +0300

    final project

--- HW2.4/ViewController.swift
+++ HW2.4/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var rGBbView: UIView!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+        
+    //MARK: - Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rGBbView.layer.cornerRadius = 8
+        setValueLabels(for: redValueLabel, greenValueLabel, blueValueLabel)
+
     }
-
-
+    
+    //MARK: - IBAction
+    @IBAction func rGBSliders(_ sender: UISlider) {
+        viewColors()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+}
+//MARK: - Extension private Functions
+extension ViewController {
+        
+    private func viewColors() {
+        rGBbView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValueLabels(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                redValueLabel.text = string(from: redSlider)
+            case greenValueLabel:
+                greenValueLabel.text = string(from: greenSlider)
+            default:
+                blueValueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
 }
 
