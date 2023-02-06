98e3f5b7ad26492f967b5f66688ee03e920bd593
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-02-06 09:12:17 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Максим Гурков on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
